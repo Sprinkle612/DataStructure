4a6c41881c593b72ae9fa33391559d0149ca1834
rainyleon <44528182+Sprinkle612@users.noreply.github.com>
2019-10-04 16:06:20 -0400

    respacing with wrong trace-back

--- HW1/respacing.py
+++ HW1/respacing.py
@@ -2,6 +2,8 @@
 # TODO: Your Partner's name, Cornell NetID
 
 # DO NOT CHANGE THIS CLASS
+
+
 class RespaceTableCell:
     def __init__(self, value, index):
         self.value = value
@@ -10,37 +12,84 @@ class RespaceTableCell:
 
     # This function allows Python to print a representation of a RespaceTableCell
     def __repr__(self):
-        return "(%s,%s)"%(str(self.value), str(self.index))
+        return "(%s,%s)" % (str(self.value), str(self.index))
 
     # Ensure everything stored is the right type and size
     def validate(self):
-        assert(type(self.value) == bool), "Values in the respacing table should be booleans."
-        assert(self.index == None or type(self.index) == int), "Indices in the respacing table should be None or int"
+        assert(type(self.value) ==
+               bool), "Values in the respacing table should be booleans."
+        assert(self.index == None or type(self.index) ==
+               int), "Indices in the respacing table should be None or int"
 
 # Inputs: the dynamic programming table, indices i, j into the dynamic programming table, the string being respaced, and an "is_word" function.
 # Returns a RespaceTableCell to put at position (i,j)
+
+
 def fill_cell(T, i, j, string, is_word):
-    #TODO: YOUR CODE HERE
+    # if i == j:
+    #     return RespaceTableCell(is_word(string[i]), None)
+    test = is_word(string[i:j+1])
+    if test:
+        return RespaceTableCell(test, None)
+    else:
+        c = 1
+        for x in range(i, j):
+            if T.get(i, x).value and T.get(i+c, j):
+                return RespaceTableCell(True, x)
+            c += 1
+
     return RespaceTableCell(False, None)
-                  
+    # return RespaceTableCell(False, None)
+
 # Inputs: N, the size of the list being respaced
 # Outputs: a list of (i,j) tuples indicating the order in which the table should be filled.
+
+
 def cell_ordering(N):
-    #TODO: YOUR CODE HERE
-    return []
+    order = []
+    for x in range(N):
+        for i in range(N-x):
+            order.append((i, i+x))
+
+    return order
 
 # Input: a filled dynamic programming table.
 # (See instructions.pdf for more on the dynamic programming skeleton)
 # Return the respaced string, or None if there is no respacing.
+
+
 def respace_from_table(s, table):
-    #TODO: YOUR CODE HERE
-    return None
+    result = ""
+    space = []
+    i, j = len(s) - 1, len(s)-1
+    prec = len(s)
+    # while i >= 0:
+    #     if table.get(i, j).value and i == prec - 1:
+    #         print("I am here!", i)
+    #         #result.insert(table.get(i, j).index, "")
+    #         if(table.get(i, j).index != None):
+    #             space.append(table.get(i, j).index)
+    #             prec = table.get(i, j).index
+    #         else:
+    #             space.append(i-1)
+    #             prec = i-1
+    #     i += -1
+
+    for c in range(len(s)):
+        result += s[c]
+        if c in space:
+            result += ' '
+    return result
+
 
 if __name__ == "__main__":
     # Example usage.
     from dynamic_programming import DynamicProgramTable
     s = "itwasthebestoftimes"
     wordlist = ["of", "it", "the", "best", "times", "was"]
-    D = DynamicProgramTable(len(s) + 1, len(s) + 1, cell_ordering(len(s)), fill_cell)
-    D.fill(string=s, is_word=lambda w:w in wordlist)
-    print respace_from_table(s, D)
+    # menu,turbo,york,cork,glen,haven,glenn,warm,dial,truck;glenndialtruckglennglen;glenn dial truck glenn glen;
+    D = DynamicProgramTable(len(s) + 1, len(s) + 1,
+                            cell_ordering(len(s)), fill_cell)
+    D.fill(string=s, is_word=lambda w: w in wordlist)
+
+    print(respace_from_table(s, D))
